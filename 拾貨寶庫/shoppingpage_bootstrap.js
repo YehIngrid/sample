8ebc9f49e243db0017b692dfe6e8463cea46e7e1
YehIngrid <ingrid171171@gmail.com>
2025-03-05 12:40:35 +0800

    製作loading動畫、處理排版問題
    往下的hover沒有滿，有時間可以修正一下

--- 拾貨寶庫/shoppingpage_bootstrap.js
+++ 拾貨寶庫/shoppingpage_bootstrap.js
@@ -1,3 +1,13 @@
+// 當整個頁面載入完成後，隱藏 loader 並顯示主要內容
+window.onload = function() {
+  // 當頁面載入完畢後隱藏載入動畫，顯示內容
+  var loader = document.getElementById('loader');
+var content = document.getElementById('whatcontent');
+if (loader && content) {
+  loader.style.setProperty('display', 'none', 'important');
+  content.style.setProperty('display', 'block', 'important');
+}
+};
 const carousel = document.querySelector('.carousel');
       const track = document.querySelector('.carousel-track');
       let containerWidth = carousel.offsetWidth;
