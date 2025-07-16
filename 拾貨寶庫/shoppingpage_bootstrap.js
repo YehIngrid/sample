df763c41c5d161541cc05f2c61aa6ddf03e8af02
YehIngrid <ingrid171171@gmail.com>
2025-07-17 02:39:53 +0800

    updateka

--- 拾貨寶庫/shoppingpage_bootstrap.js
+++ 拾貨寶庫/shoppingpage_bootstrap.js
@@ -1,15 +1,14 @@
+var content = document.getElementById('whatcontent');
 // 當整個頁面載入完成後，隱藏 loader 並顯示主要內容
 window.onload = function() {
   // 當頁面載入完畢後隱藏載入動畫，顯示內容
   var loader = document.getElementById('loader');
-var content = document.getElementById('whatcontent');
+
 if (loader && content) {
   loader.style.setProperty('display', 'none', 'important');
   content.style.setProperty('display', 'block', 'important');
 }
 };
-
-
 //JavaScript: 控制左右按鈕捲動
 
   const scrollContainer = document.getElementById('scrollContainer');
