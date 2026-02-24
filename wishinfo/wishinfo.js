8e9bc32169bc728409ca0ebe973b2ad7ff037c13
YehIngrid <ingrid171171@gmail.com>
2026-02-24 11:13:16 +0800

    delete light box

--- wishinfo/wishinfo.js
+++ wishinfo/wishinfo.js
@@ -83,21 +83,3 @@ async function handleContactWisher() {
     btn.addEventListener('click', handleContactWisher);
   }
 });
-
-
-// 綁定點擊事件
-document.querySelectorAll('.image-item img').forEach(img => {
-    img.addEventListener('click', () => {
-        openLightbox(img.src);
-    });
-});
-
-// ESC 鍵關閉
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape') closeLightbox();
-});
-
-// 點擊背景關閉
-document.getElementById('lightbox').addEventListener('click', (e) => {
-    if (e.target.id === 'lightbox') closeLightbox();
-});
